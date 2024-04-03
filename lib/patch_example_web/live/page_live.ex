883ab1be64317526120dccf53c1b4bc73ce96ba9
BrooklinJazz <brooklinjazzmyers@gmail.com>
2024-04-03 16:42:27 -0400

    add multiple handle_params with pattern matching

--- lib/patch_example_web/live/page_live.ex
+++ lib/patch_example_web/live/page_live.ex
@@ -3,7 +3,11 @@ defmodule PatchExampleWeb.PageLive do
 
   def mount(_params, _session, socket) do
     IO.inspect("MOUNTING")
-    {:ok, assign(socket, mount_id: Enum.random(1..100))}
+    {:ok, assign(socket, mount_id: Enum.random(1..100), patch_id: Enum.random(1..100), color: "none")}
+  end
+
+  def handle_params(%{"color" => color}, _uri, socket) do
+    {:noreply, assign(socket, color: color, patch_id: Enum.random(1..100))}
   end
 
   def handle_params(params, _uri, socket) do
@@ -15,11 +19,20 @@ defmodule PatchExampleWeb.PageLive do
     ~H"""
     <p>Mounted:<%= @mount_id %></p>
     <p>Patched:<%= @patch_id %></p>
+    <p>Color:<%= @color %></p>
     <.button>
       <.link navigate={"/"}>navigate (re-mount)</.link>
     </.button>
     <.button>
       <.link patch={"/"}>patch</.link>
+    </.button>
+
+    <.button>
+      <.link patch={"/?color=red"}>color red patch</.link>
+    </.button>
+
+    <.button>
+      <.link patch={"/?color=blue"}>color blue patch</.link>
     </.button>
 
     <h1 class="mt-10">Parent LiveView Assigns:</h1>
